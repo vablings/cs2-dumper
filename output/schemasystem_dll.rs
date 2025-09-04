6a9458d8ae6d2922d161d37fa68577395536db0c
vablings <47798035+vablings@users.noreply.github.com>
2025-09-03 20:42:30 -0500

    Game update (14099)

--- output/schemasystem_dll.rs
+++ output/schemasystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-08-28 05:57:23.340171200 UTC
+// 2025-09-04 01:39:39.888237100 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
